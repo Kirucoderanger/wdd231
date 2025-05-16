098e49d34dc4a7eec4e384992cc4fbe395983475
Kirucoderanger <klemu@byupathway.edu>
2025-05-16 19:45:01 +0300

    animated menu added

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,5 +1,39 @@
 // script.js
+const menuItem = document.querySelector("#menu");
+const navElement = document.querySelector("#animated-menu");
 
+// Toggle navigation menu on mobile
+menuItem.addEventListener("click", () => {
+    //navElement.classList.toggle("responsive");
+    navElement.classList.toggle("open");
+    menuItem.classList.toggle("open");
+
+});
+// Close navigation menu when a link is clicked
+/*const navLinks = document.querySelectorAll(".navigation a");
+navLinks.forEach(link => {  
+    link.addEventListener("click", () => {
+        navElement.classList.remove("responsive");
+    });
+});
+// Update the year and last modified date
+const yearSpan = document.querySelector("#year");
+const lastModifiedSpan = document.querySelector("#lastModified");
+yearSpan.textContent = new Date().getFullYear();
+lastModifiedSpan.textContent = document.lastModified;
+// Toggle between grid and list view for members    
+const gridBtn = document.querySelector("#grid-view");
+const listBtn = document.querySelector("#list-view");
+const membersContainer = document.querySelector("#members-container");
+gridBtn.addEventListener("click", () => {
+    membersContainer.classList.add("grid-view");
+    membersContainer.classList.remove("list-view");
+});
+listBtn.addEventListener("click", () => {   
+    membersContainer.classList.add("list-view");
+    membersContainer.classList.remove("grid-view");
+});
+*/
 document.addEventListener("DOMContentLoaded", () => {
   const yearSpan = document.getElementById("year");
   const lastModifiedSpan = document.getElementById("lastModified");
